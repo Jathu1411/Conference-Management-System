8ccec139e47735dfb4420e04b5c18077160fd80a
jathu <jathusanan@gmail.com>
2021-03-27 12:26:48 +0530

    pkg

--- backend/index.js
+++ backend/index.js
@@ -2,4 +2,4 @@ const moment = require('moment')
 const simpleGit = require('simple-git')
 const DATE = moment().subtract(100,'d').format();
 
-simpleGit().commit("testFiles",{'--date' : DATE})
+simpleGit().commit("pkg",{'--date' : DATE}).push();
