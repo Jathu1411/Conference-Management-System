08b1c2f310827a57ef0e03f2657aa63fe4275eaa
jathu <jathusanan@gmail.com>
2021-03-27 12:25:52 +0530

    testFiles

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 const moment = require('moment')
 const simpleGit = require('simple-git')
-const DATE = moment().format();
+const DATE = moment().subtract(100,'d').format();
 
 simpleGit().commit("testFiles",{'--date' : DATE})
